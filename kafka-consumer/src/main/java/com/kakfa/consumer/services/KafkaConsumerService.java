cca5af5922cffb7e22c9c2f54d368a461b4c5597
Vivek Agrawal <vivek.agrawal01@sap.com>
2024-07-04 00:29:49 +0530

    Docker changes

---
+++ kafka-consumer/src/main/java/com/kakfa/consumer/services/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package com.kakfa.consumer.services;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
